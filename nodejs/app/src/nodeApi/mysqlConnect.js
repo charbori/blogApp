58d55fe16d480482370fd2f588a901464ab20783
root <root@ip-172-31-42-99.ap-northeast-2.compute.internal>
2021-06-13 06:04:56 +0000

    set mysqlconnect on env

--- nodejs/app/src/nodeApi/mysqlConnect.js
+++ nodejs/app/src/nodeApi/mysqlConnect.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql');
+console.log("mysql connect db:" + process.env.DB_HOST
+    + " " + process.env.DB_USER
+    + " " + process.env.DB_PASS
+    + " " + process.env.DB_NAME);
 const connection = mysql.createConnection({
-    host:'mysqldb-dev.csprrtzyvhjo.ap-northeast-2.rds.amazonaws.com',
-    user:'admin',
-    password:'lazy!00$girl',
-    database:'chatApp'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database:process.env.DB_NAME
 });
 
 module.exports = connection;
--- nodejs/app/src/server/server.js
+++ nodejs/app/src/server/server.js
@@ -1,29 +1,17 @@
-import path from "path";
-import dotenv from 'dotenv';
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.POST || 8888;
+import "../../env/env.js";
+
 const api = require('../routes/index');
-const apiBook = require('../routes/booksRouter');
 const apiBoard = require('../routes/boardRouter');
 const cors = require('cors');
-
-if (process.env.NODE_ENV === 'prod') {
-	dotenv.config({ path: path.join("env", ".env.prod") })
-} else if (process.env.NODE_ENV === 'dev') {
-	dotenv.config({ path: path.join("env", ".env.dev") })
-} else {
-	throw new Error("process.env.NODE_ENV를 설정하지 않았습니다.")
-}
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', api);
-app.use('/book', apiBook);
-app.use('/board', apiBoard);
-//app.use('/api', (req, res) => res.json({username:'bryan'}));
+app.use('/api/board', apiBoard);
 
 app.listen(port, () => {
     console.log(`express is running on ${port}`);
