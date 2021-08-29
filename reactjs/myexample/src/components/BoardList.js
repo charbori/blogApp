73f337beab63364960b448cac9b72baf545d58a3
jaehyeok <charbori@github.com>
2021-08-29 21:14:25 +0900

    ci test

--- reactjs/myexample/src/components/BoardList.js
+++ reactjs/myexample/src/components/BoardList.js
@@ -30,7 +30,7 @@ const BoardList = (user_data, post_data, content_data) => {
                             <i className="fa fa-sort-asc" aria-hidden="true"></i>
                         </Row>
                         <Row>
-                            <span>{like}</span>
+                            <span>{like}10</span>
                         </Row>
                         <Row>
                             <i className="fa fa-sort-desc" aria-hidden="true"></i>
