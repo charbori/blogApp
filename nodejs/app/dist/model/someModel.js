882221611df8d0ed71364611947bef357e472c5a
jaehyeok <couhensoft@gmail.com>
2021-09-12 23:01:10 +0900

    delete dist folder

--- nodejs/app/dist/model/someModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var mongo = require('mongoose');
-
-var Schema = mongo.Schema;
-var SomeModelSchema = new Schema({
-  name: String
-});
-module.exports = mongo.model('SomeModel', SomeModelSchema);
