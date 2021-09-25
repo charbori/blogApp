d114cc36fbcf559b545f2fb1e569ad37eadd38d0
jaehyeok <couhensoft@gmail.com>
2021-09-25 18:23:51 +0900

    add package.json

--- nodejs/app/src/server/server.js
+++ nodejs/app/src/server/server.js
@@ -10,6 +10,7 @@ import apiBoard from '../routes/boardRouter';
 import apiLogs from '../routes/logsRouter';
 import apiMenu from '../routes/menu';
 import cors from 'cors';
+import login from '../routes/login'
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,6 +18,7 @@ app.use('/api', api);
 app.use('/api/board', apiBoard);
 app.use('/api/logs', apiLogs);
 app.use('/api/category', apiMenu);
+app.use('/api/auth', login);
 
 app.listen(port, () => {
     console.log(`express is running on ${port}`);
