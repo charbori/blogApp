33fdd528e8b6b6af369b0827ecfdb46102f42090
jaehyeok <couhensoft@gmail.com>
2021-11-28 01:55:34 +0900

    webpack css loader not exclude node_module dir \n add quill text editor

--- reactjs/myexample/webpack.config.js
+++ reactjs/myexample/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 			},
 		    {
 				test: /\.(css|scss)$/,
-				exclude: /node_modules/,
+//				exclude: /node_modules/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
 		    },
 		    {
