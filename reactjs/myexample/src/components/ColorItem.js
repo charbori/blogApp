33b612ff6e7519cd7c16789b1291883004580e02
jaehyeok <couhensoft@gmail.com>
2021-12-11 02:04:51 +0900

    ci/cd test

--- reactjs/myexample/src/components/ColorItem.js
+++ reactjs/myexample/src/components/ColorItem.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import './ColorItem.css';
 
 const ColorItem = ({color, onColorChange, colorItemStyle}) => {
-
     var styleObj = { background: color };
-
     return (
         <div className="colorItemContent" style={ styleObj }
         onClick={() => onColorChange(color)}>
