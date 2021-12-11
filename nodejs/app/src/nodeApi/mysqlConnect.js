cd828cd74c818065267c638939c27ba4f7c10036
jaehyeok <couhensoft@gmail.com>
2021-12-12 02:37:04 +0900

    mod nodejs env

--- nodejs/app/src/nodeApi/mysqlConnect.js
+++ nodejs/app/src/nodeApi/mysqlConnect.js
@@ -1,5 +1,14 @@
 const mysql = require('mysql2');
 
+import envList from '../.env';
+
+if (process.env.NODE_ENV == 'dev') {
+    process.env.DB_HOST = envList.DB_HOST;
+    process.env.DB_PASS = envList.DB_PASS;
+    process.env.DB_NAME = envList.DB_NAME;
+    process.env.DB_USER = envList.DB_USER;
+}
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
